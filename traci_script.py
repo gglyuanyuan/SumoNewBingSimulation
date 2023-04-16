c619e47bb1627ae0f9bd334fb68fde631de1e645
gglyuanyuan <39005517+gglyuanyuan@users.noreply.github.com>
2023-04-16 18:17:30 +0800

    Update traci_script.py

--- traci_script.py
+++ traci_script.py
@@ -1,61 +1,58 @@
+import os
 import traci
-import traci.constants as tc
 import sys
 sys.path.append("D:/Program_WorkApplication/Eclipse/Sumo/tools")
 
-# start sumo with gui and load the network and route files
+# set the path of sumo binary
 sumoBinary = "sumo-gui"
 sumoCmd = [sumoBinary, "-c", "sumo.cfg"]
+
+# start the simulation
 traci.start(sumoCmd)
 
-# subscribe to the emergency vehicles and get their ids
-traci.vehicle.subscribeContext("emergency_flow", tc.CMD_GET_VEHICLE_VARIABLE, 100, [tc.VAR_SPEED])
-emergency_ids = traci.vehicle.getContextSubscriptionResults("emergency_flow")
+# get the list of emergency vehicles
+emergency_vehicles = traci.vehicle.getIDList()
+emergency_vehicles = [v for v in emergency_vehicles if v.startswith("emergency")]
 
-# loop until the simulation ends
-while traci.simulation.getMinExpectedNumber() > 0:
+# get the list of social vehicles
+social_vehicles = traci.vehicle.getIDList()
+social_vehicles = [v for v in social_vehicles if v.startswith("social")]
+
+# set the simulation step length
+step_length = 0.1
+
+# run the simulation for 1000 steps
+for i in range(1000):
     # advance the simulation by one step
     traci.simulationStep()
-    
-    # get the current time
-    time = traci.simulation.getTime()
-    
-    # loop over the emergency vehicles
-    for eid in emergency_ids:
-        # get the speed of the emergency vehicle
-        espeed = traci.vehicle.getSubscriptionResults(eid)[tc.VAR_SPEED]
-        
-        # get the surrounding vehicles within 100 meters
-        neighbors = traci.vehicle.getContextSubscriptionResults(eid)
-        
-        # loop over the surrounding vehicles
-        for nid in neighbors:
-            # get the speed of the neighbor vehicle
-            nspeed = neighbors[nid]  
-            # if the neighbor vehicle is a social vehicle and is slower than the emergency vehicle
-            if nid.startswith("social") and nspeed < espeed:
-                # get the lane index of the neighbor vehicle
-                nlane = traci.vehicle.getLaneIndex(nid)
-                
-                # if the neighbor vehicle is on the leftmost lane
-                if nlane == 0:
-                    # change its lane to the right
-                    traci.vehicle.changeLane(nid, 1, 10)
-                
-                # if the neighbor vehicle is on the middle lane
-                elif nlane == 1:
-                    # get the right lane vehicles
-                    rlane_vehicles = traci.lane.getLastStepVehicleIDs("e1_2")
-                    
-                    # if there is no vehicle on the right lane or the gap is large enough
-                    if len(rlane_vehicles) == 0 or traci.vehicle.getLeader(nid, 2.5)[1] > 10:
-                        # change its lane to the right
-                        traci.vehicle.changeLane(nid, 2, 10)
-                
-                # if the neighbor vehicle is on the rightmost lane
-                else:
-                    # slow down to let the emergency vehicle pass
-                    traci.vehicle.slowDown(nid, nspeed - 5, 5)
 
-# close the connection to sumo
+    # loop through all emergency vehicles
+    for ev in emergency_vehicles:
+        # get the current lane and position of the emergency vehicle
+        ev_lane = traci.vehicle.getLaneID(ev)
+        ev_pos = traci.vehicle.getLanePosition(ev)
+
+        # loop through all social vehicles
+        for sv in social_vehicles:
+            # get the current lane and position of the social vehicle
+            sv_lane = traci.vehicle.getLaneID(sv)
+            sv_pos = traci.vehicle.getLanePosition(sv)
+
+            # check if the social vehicle is on the same edge as the emergency vehicle
+            if ev_lane.split("_")[0] == sv_lane.split("_")[0]:
+                # check if the social vehicle is ahead of the emergency vehicle and within 50 meters
+                if sv_pos > ev_pos and sv_pos - ev_pos < 50:
+                    # check if the social vehicle is on the left lane of the emergency vehicle
+                    if int(sv_lane.split("_")[1]) < int(ev_lane.split("_")[1]):
+                        # try to change the lane of the social vehicle to the right
+                        traci.vehicle.changeLane(sv, int(sv_lane.split("_")[1]) + 1, 5)
+                    # check if the social vehicle is on the same lane as the emergency vehicle
+                    elif int(sv_lane.split("_")[1]) == int(ev_lane.split("_")[1]):
+                        # try to change the lane of the social vehicle to the left or right, depending on availability
+                        if traci.vehicle.couldChangeLane(sv, -1):
+                            traci.vehicle.changeLane(sv, int(sv_lane.split("_")[1]) - 1, 5)
+                        elif traci.vehicle.couldChangeLane(sv, 1):
+                            traci.vehicle.changeLane(sv, int(sv_lane.split("_")[1]) + 1, 5)
+
+# close the simulation
 traci.close()
